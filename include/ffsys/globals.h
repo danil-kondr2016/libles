c6fbdfad880ddc7a61b972f24a5fbd2659bec117
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2023-11-15 19:45:13 +0600

    [ffsys] Удалены лишние файлы

--- include/ffsys/globals.h
+++ include/ffsys/globals.h
@@ -5,22 +5,8 @@ int _ffcpu_features;
 
 #ifdef FF_WIN
 
-#include <ffsys/dylib.h>
-signed char _ffdl_open_supports_flags;
-
 #include <ffsys/error.h>
 char _fferr_buffer[1024];
-
-#include <ffsys/socket.h>
-LPFN_DISCONNECTEX _ff_DisconnectEx;
-LPFN_CONNECTEX _ff_ConnectEx;
-LPFN_ACCEPTEX _ff_AcceptEx;
-LPFN_GETACCEPTEXSOCKADDRS _ff_GetAcceptExSockaddrs;
-
-#include <ffsys/perf.h>
-LARGE_INTEGER _fftime_perffreq;
-_ff_NtQuerySystemInformation_t _ff_NtQuerySystemInformation;
-_ff_GetProcessMemoryInfo_t _ff_GetProcessMemoryInfo;
 
 #else // UNIX:
 
@@ -28,6 +14,3 @@ _ff_GetProcessMemoryInfo_t _ff_GetProcessMemoryInfo;
 char **_ff_environ;
 
 #endif
-
-#include <ffsys/signal.h>
-ffsig_handler _ffsig_userhandler;
