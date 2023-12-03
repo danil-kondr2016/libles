a2492f5e0b40e4c0bffb28fead5fc12c8637d30c
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2023-12-03 15:41:05 +0600

    [les] v0.1.2: исправление определения версий

--- include/les/version.h
+++ include/les/version.h
@@ -4,18 +4,6 @@
 
 #include <les/public.h>
 
-#define LES_MAJOR_VERSION 0
-#define LES_MINOR_VERSION 1
-#define LES_PATCH_VERSION 1
-
-#define XSTR(x) #x
-#define STR(x) XSTR(x)
-
-#define LES_VERSION \
-	    STR(LES_MAJOR_VERSION) \
-	"." STR(LES_MINOR_VERSION) \
-	"." STR(LES_PATCH_VERSION)
-
 LIBLES_API
 const char *les_version();
 
--- les/version.c
+++ les/version.c
@@ -1,6 +1,18 @@
 #define _LES_DLL
 #include <les/version.h>
 
+#define LES_MAJOR_VERSION 0
+#define LES_MINOR_VERSION 1
+#define LES_PATCH_VERSION 2
+
+#define XSTR(x) #x
+#define STR(x) XSTR(x)
+
+#define LES_VERSION \
+	    STR(LES_MAJOR_VERSION) \
+	"." STR(LES_MINOR_VERSION) \
+	"." STR(LES_PATCH_VERSION)
+
 static const char __version[] = LES_VERSION;
 
 LIBLES_API
